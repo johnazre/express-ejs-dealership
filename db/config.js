2a691f604085882bf0d72c921d817de5d32ce09d
JohnA <jaboyak@gmail.com>
2017-06-22 10:12:22 -0700

    added dummy data vehicles to config file

--- db/config.js
+++ db/config.js
@@ -2,6 +2,34 @@ var Loki = require('lokijs');
 var db = new Loki('loki.json');
 var vehicles = db.addCollection('vehicles');
 
+vehicles.insert([
+  {
+    year: 2013,
+    make: "Honda",
+    model: "Accord LX"
+  },
+  {
+    year: 2015,
+    make: "GMC",
+    model: "Sierra"
+  },
+  {
+    year: 2013,
+    make: "Honda",
+    model: "Civic DX"
+  },
+  {
+    year: 2010,
+    make: "Chevy",
+    model: "Cavalier"
+  },
+  {
+    year: 2017,
+    make: "Chevy",
+    model: "Volt"
+  },
+])
+
 module.exports = {
   vehicles: vehicles
 };
